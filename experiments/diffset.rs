b975630cefd804673816d5909db8e842f18b8161
Wai-Shing Luk <luk036@gmail.com>
2024-01-10 11:09:15 +0800

    improve diffset.rs

--- experiments/diffset.rs
+++ experiments/diffset.rs
@@ -1,18 +1,18 @@
 // use std::mem;
 use std::process::exit;
 
-const MAX: usize = 100;
-const MAX_N: usize = 100;
+const MAX: usize = 20;
+const MAX_N: usize = 70;
 
 struct DiffCover {
-    n: i32,
-    d: i32,
-    a: [i32; MAX],
-    b: [i32; MAX],
-    threshold: i32,
-    n_minus_d: i32,
-    n1: i32,
-    n2: i32,
+    n: usize,
+    d: usize,
+    a: [usize; MAX],
+    b: [usize; MAX],
+    threshold: usize,
+    n_minus_d: usize,
+    n1: usize,
+    n2: usize,
     size_n: usize,
 }
 
@@ -20,24 +20,24 @@ impl DiffCover {
 
     fn print_d(&self) {
         for i in 1..=self.d {
-            print!("{} ", self.a[i as usize]);
+            print!("{} ", self.a[i]);
         }
         println!();
         exit(0);
     }
 
-    fn gen_d(&mut self, t: i32, p: i32, tt: i32, diffset: &mut [i8; MAX_N]) {
+    fn gen_d(&mut self, t: usize, p: usize, tt: usize, diffset: &mut [i8; MAX_N]) {
         let mut differences = [0; MAX_N];
         differences[..self.size_n].copy_from_slice(&diffset[..self.size_n]);
         // let mut differences = diffset.to_owned();
         for i in 0..t {
-            let diff = self.a[t as usize] - self.a[i as usize];
-            differences[std::cmp::min(diff, self.n - diff) as usize] = 1;
+            let diff = self.a[t] - self.a[i];
+            differences[std::cmp::min(diff, self.n - diff)] = 1;
         }
         if t >= self.threshold {
             let mut count = 0;
             for i in 1..=self.n2 {
-                if differences[i as usize] != 0 {
+                if differences[i] != 0 {
                     count += 1;
                 }
             }
@@ -50,21 +50,21 @@ impl DiffCover {
             self.print_d();
         } else {
             let mut tail = self.n_minus_d + t1;
-            let max = self.a[t1 as usize - p as usize] + self.a[p as usize];
+            let max = self.a[t1 - p] + self.a[p];
             let tt1 = t1 * (t1 + 1) / 2;
             if max <= tail {
-                self.a[t1 as usize] = max;
-                self.b[t1 as usize] = self.b[t1 as usize - p as usize];
+                self.a[t1] = max;
+                self.b[t1] = self.b[t1 - p];
                 self.gen_d(t1, p, tt1, &mut differences);
-                if self.b[t1 as usize] == 0 {
-                    self.b[t1 as usize] = 1;
+                if self.b[t1] == 0 {
+                    self.b[t1] = 1;
                     self.gen_d(t1, t1, tt1, &mut differences);
                 }
                 tail = max - 1;
             }
-            for j in (self.a[t as usize] + 1..=tail).rev() {
-                self.a[t1 as usize] = j;
-                self.b[t1 as usize] = 1;
+            for j in (self.a[t] + 1..=tail).rev() {
+                self.a[t1] = j;
+                self.b[t1] = 1;
                 self.gen_d(t1, t1, tt1, &mut differences);
             }
         }
@@ -72,9 +72,9 @@ impl DiffCover {
 
     fn init(&mut self) {
         // for j in 0..=self.d {
-        //     self.a[j as usize] = 0;
+        //     self.a[j] = 0;
         // }
-        self.a[self.d as usize] = self.n;
+        self.a[self.d] = self.n;
         self.a[0] = 0;
         let mut differences = [0; MAX_N];
         differences[0] = 1;
@@ -97,9 +97,9 @@ fn main() {
         usage();
         return;
     }
-    let n: i32 = args[1].parse().unwrap();
-    let d: i32 = args[2].parse().unwrap();
-    let threshold: i32 = args[3].parse().unwrap();
+    let n: usize = args[1].parse().unwrap();
+    let d: usize = args[2].parse().unwrap();
+    let threshold: usize = args[3].parse().unwrap();
     if n > d * (d - 1) + 1 {
         println!("Error: N must be less than D*(D-1)+1");
         return;
@@ -113,9 +113,9 @@ fn main() {
         n_minus_d: n - d,
         n1: n / 2 - d * (d - 1) / 2,
         n2: n / 2,
-        size_n: (n / 2 + 1) as usize,
+        size_n: (n / 2 + 1),
     };
-    diffcover.a[d as usize] = n;
+    diffcover.a[d] = n;
     diffcover.a[0] = 0;
     diffcover.init();
 }
